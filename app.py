15e2d205776abb428ae6ee699e6936da8f71cd3e
Chrisvann1 <Christophervann123@gmail.com>
2024-04-06 13:31:14 -0400

    These changes are the result of me fixing errors after doing some testing

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ def init_db():
     conn = sqlite3.connect("note.db")
     cursor = conn.cursor()
     cursor.execute("""CREATE TABLE IF NOT EXISTS notes(
-    id INTEGER,
     title TEXT,
     content TEXT, 
     modified_date INTEGER,
@@ -38,18 +37,25 @@ def create_note():
     payload = request.json
     try: 
         title = payload.get('title')
-    except KeyError:
-        abort(206, "Data Entry Requires A Title")
-    
+        if title is None: 
+            raise KeyError("Date Entry Requires A Title")
+    except KeyError as error:
+        abort(206, error)
+        
     try: 
         content = payload.get('content')
     except KeyError: 
         content = ""
     
     cursor.execute("INSERT INTO notes VALUES (?,?,?,?)", (title,content,date.today(),date.today()))
-
+    conn.commit()
     conn.close()
-        
+    return "Note Created Successfully", 200
+
+#{"title": "information", "tag": ["things", "to", "do"]}
+#We need to eventually develop a safeguard for this. We need to make
+#sure that this title doesn't exist
+#We can proabably do this in the second sprint if needed
 @app.route('/tags', methods=['POST'])
 def create_tag(): 
     conn = sqlite3.connect("note.db")
@@ -57,16 +63,24 @@ def create_tag():
     payload = request.json
     try: 
         title = payload.get('title')
-    except KeyError:
-        abort(206, "Data Entry Requires A Title")
+        if title is None: 
+            raise KeyError("Date Entry Requires A Title")
+    except KeyError as error:
+        abort(206, error)
 
     try: 
         tags = payload.get('tag')
-    except KeyError: 
-        abort(206, "Data Entry Requires a tag")
+        if tags is None: 
+            raise KeyError("Date Entry Requires a tag")
+    except KeyError as error: 
+        abort(206, error)
         
     for value in tags: 
         cursor.execute("INSERT INTO tags VALUES (?,?)", (title, value))
+    
+    conn.commit()
+    conn.close()
+    return "Tag(s) Created Successfully", 200
 
 #used to delete notes and delete tags
 @app.route('/notes', methods=['DELETE'])
@@ -76,14 +90,18 @@ def delete_note():
     payload = request.json
     try: 
         title = payload.get('title')
-    except KeyError:
-        abort(206, "Data Entry Requires A Title")
+        if title is None: 
+            raise KeyError("Data Entry Requires a title")
+    except KeyError as error:
+        abort(206, error)
 
-    cursor.execute(f"DELETE FROM tags WHERE title == {title}")
-    cursor.execute(f"DELETE FROM notes WHERE title == {title}")
+    cursor.execute(f"DELETE FROM tags WHERE tags.title == '{title}'")
+    cursor.execute(f"DELETE FROM notes WHERE notes.title == '{title}'")
     conn.commit()   
     conn.close()
+    return "Note Deleted Successfully", 200
 
+#for tags you need a list in json format
 @app.route('/tags', methods=['DELETE'])
 def delete_tag():
     conn = sqlite3.connect("note.db")
@@ -91,18 +109,23 @@ def delete_tag():
     payload = request.json
     try: 
         title = payload.get('title')
-    except KeyError:
-        abort(206, "Data Entry Requires A Title")
+        if title is None: 
+            raise KeyError("Data Entry Requires a title")
+    except KeyError as error:
+        abort(206, error)
     
     try: 
         tags = payload.get('tag')
-    except KeyError: 
-        abort(206, "Data Entry Requires A Tag")
+        if tag is None: 
+            raise KeyError("Data Entry Requires a tag")
+    except KeyError as error: 
+        abort(206, error)
         
     for tag in tags: 
-        cursor.execute(f"DELETE FROM tags WHERE title == {title} AND tag == {tag}")
+        cursor.execute(f"DELETE FROM tags WHERE title == '{title}' AND tag == '{tag}'")
     conn.commit()    
     conn.close()
+    return "Tag(s) Deleted Successfully", 200
 
 #used to update note content
 @app.route('/notes', methods=['PUT'])
@@ -113,17 +136,22 @@ def update_note():
     
     try: 
         title = payload.get('title')
-    except KeyError:
-        abort(206, "Data Entry Requires A Title")
+        if title is None: 
+            raise KeyError("Data Entry Requires a title")
+    except KeyError as error:
+        abort(206, error)
         
     try: 
         content = payload.get('content')
-    except KeyError: 
-        abort(206, "Data Entry Requires Content")
+        if content is None: 
+            raise KeyError("Data Entry Requires content")
+    except KeyError as error: 
+        abort(206, error)
         
-    cursor.execute(f"UPDATE notes SET content = {content}, modified_date = {date.today()} WHERE title = {title}")
+    cursor.execute(f"UPDATE notes SET content == '{content}', modified_date == '{date.today()}' WHERE title == '{title}'")
     conn.commit()
     conn.close()
+    return "Content Updated Successfully", 200
     
 
 
@@ -176,7 +204,7 @@ def search_notes():
         sql_query = f"""
                    SELECT {select_fields_string}
                    FROM notes
-                   WHERE notes.title = {query}
+                   WHERE notes.title = '{query}'
                    """
     # created_date looks like 'YYYY-MM-DD'
     if search_field == 'created_date':
@@ -200,15 +228,19 @@ def search_notes():
                    FROM notes
                    INNER JOIN tags AS tg
                    ON notes.title = tg.title
-                   WHERE tg.tag = {query}
+                   WHERE tg.tag = '{query}'
                    """
     cursor.execute(sql_query)
     fetch = cursor.fetchall()
     conn.close()
     return fetch
 
-
+#{"list_field":"title"}
 ###LISTING FUNCTIONS
+#There are problems here. Currently, the list by modified date and list by created 
+#date just lists all of the titles rather than listing them based on 
+#the chronology of their dates. I am not sure if this is a backend or frontend problem
+#but I thought it would be useful to leave a note here
 @app.route('/notes/list', methods=['GET'])
 def list():
     conn = sqlite3.connect("note.db")
@@ -221,7 +253,7 @@ def list():
     # handle the fact that title is always returned
     # and you will NOT be SQL injecting this shit
     if list_field == 'title':
-        list_field = ','
+        list_field = ''
     else:
         list_field = ", notes." + list_field
     sql_query = f"""
@@ -239,7 +271,7 @@ def list_tags():
     conn = sqlite3.connect("note.db")
     cursor = conn.cursor()
     sql_query = f"""
-                    SELECT tags.tag AND tags.title
+                    SELECT tags.tag, tags.title
                     FROM tags
                 """
     cursor.execute(sql_query)
@@ -248,3 +280,4 @@ def list_tags():
     conn.close()
     return fetch 
      
+app.run(debug=True)
