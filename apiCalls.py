816f2abca3c41e6f0a1fbf197426681e32c1fb07
Andrew Fischer <atb.fischer@gmail.com>
2024-04-25 15:52:05 -0400

    Tried fixing a minor problem that got merged in. Failed, but it doesn't matter

--- apiCalls.py
+++ apiCalls.py
@@ -18,7 +18,7 @@ def listNotes(listBy):
 	url = "http://127.0.0.1:5000/notes/list"
 	return requests.get(url,headers=header, json={'list_field': listBy})
 
-def searchNotes(field, searchQuery, returnField, start='1970-01-01', stop=today.strftime("%Y-%m-%d")):
+def searchNotes(field, searchQuery, returnField, start='1970-01-01', stop=date.today().strftime("%Y-%m-%d")):
 	#by content, title, tags, date
 	url = "http://127.0.0.1:5000/notes/search"
 	# conditional check on whether to pass start and stop
